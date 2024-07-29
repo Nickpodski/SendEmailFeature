089d926c7cdd79009d4f3f5f1cf3b4d7dc1aca7a
Nicholas Podniestrzanski <nickpodski@gmail.com>
2024-07-28 23:14:23 -0400

    Started on the console app

---
+++ SendEmailFeature.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SendEmailFeature.Lib;
+
+var host = Host.CreateApplicationBuilder();
+host.Configuration.AddJsonFile("appsettings.Development.json", true, true);
+
+host.Services.AddTransient<EmailSender>();
+
+var app = host.Build();
+
+var emailSender = app.Services.GetService<EmailSender>();
+
+emailSender?.TrySendAsync("nickpodski@gmail.com");
+
