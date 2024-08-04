3bb4bddb8a6e96d42f4a06e5d38cf6b6049ad50c
Nicholas Podniestrzanski <nickpodski@gmail.com>
2024-08-04 18:18:26 -0400

    Updated git ignore and added loggins to the console application.

--- SendEmailFeature.Console/Program.cs
+++ SendEmailFeature.Console/Program.cs
@@ -1,7 +1,19 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SendEmailFeature.Lib;
+
+var root = GetProjectDirectory();
+
+var listener = new TextWriterTraceListener(@$"{root}\logs.txt");
+var sourceSwitch = new SourceSwitch("sourceSwitch", "Logging Sample")
+{
+    Level = SourceLevels.Information
+};
+
 
 var host = Host.CreateDefaultBuilder();
 host.ConfigureAppConfiguration((hostingContext, configuration) =>
@@ -13,6 +25,10 @@ host.ConfigureAppConfiguration((hostingContext, configuration) =>
 host.ConfigureServices((context, services) =>
 {
     services.AddTransient<EmailSender>();
+});
+
+host.ConfigureLogging((loggerBuilder) => {
+    loggerBuilder.AddTraceSource(sourceSwitch, listener);
 });
 
 var app = host.Build();
@@ -27,3 +43,7 @@ while (string.IsNullOrEmpty(email)) {
 }
 
 await emailSender.TrySendAsync(email);
+listener.Flush();
+
+static string? GetProjectDirectory([CallerFilePath] string sourceFilePath = "") 
+    => Path.GetDirectoryName(sourceFilePath);
